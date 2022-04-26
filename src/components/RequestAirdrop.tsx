dc614304e3adb2863caf26d2c0338ae8755cec33
Sina <sinarahimi.main@example.com>
2022-04-27 00:25:52 +0430

    initial commit

---
+++ src/components/RequestAirdrop.tsx
@@ -0,0 +1,46 @@
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { LAMPORTS_PER_SOL, TransactionSignature } from '@solana/web3.js';
+import { FC, useCallback } from 'react';
+import { notify } from "../utils/notifications";
+import useUserSOLBalanceStore from '../stores/useUserSOLBalanceStore';
+const web3 = require('@solana/web3.js');
+
+export const RequestAirdrop: FC = () => {
+    const { connection } = useConnection();
+    const { publicKey } = useWallet();
+    const { getUserSOLBalance } = useUserSOLBalanceStore();
+
+    const onClick = useCallback(async () => {
+        if (!publicKey) {
+            console.log('error', 'Wallet not connected!');
+            notify({ type: 'error', message: 'error', description: 'Wallet not connected!' });
+            return;
+        }
+
+        let base58publicKey = new web3.PublicKey('Djn8WHX1TcYEc7v4smKWnJbCMhbgMosaFpcFDgvEAc2U');
+        let signature: TransactionSignature = '';
+
+        try {
+            signature = await connection.requestAirdrop(base58publicKey, LAMPORTS_PER_SOL);
+            await connection.confirmTransaction(signature, 'confirmed');
+            notify({ type: 'success', message: 'Airdrop successful!', txid: signature });
+
+            getUserSOLBalance(base58publicKey, connection);
+        } catch (error: any) {
+            notify({ type: 'error', message: `Airdrop failed!`, description: error?.message, txid: signature });
+            console.log('error', `Airdrop failed! ${error?.message}`, signature);
+        }
+    }, [publicKey, connection, getUserSOLBalance]);
+
+    return (
+        <div>
+            <button
+                className="px-8 m-2 btn animate-pulse bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ..."
+                onClick={onClick}
+            >
+                <span>Airdrop 1 </span>
+            </button>
+        </div>
+    );
+};
+
